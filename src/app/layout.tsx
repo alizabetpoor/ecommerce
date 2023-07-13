cc241e0f2a5237f5dd2b94c41a28365f9c4ce0ab
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2023-07-13 13:26:19 +0330

    (1)feat/config_redux install and config redux add iransans font add redux provider to the rootlayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import "./globals.css";
+import Providers from "@/redux/provider";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fa">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <Providers>
+      <html lang="fa">
+        <body className="font-iranSans">{children}</body>
+      </html>
+    </Providers>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="">test</main>;
+  return <main className="">سلام</main>;
 }
